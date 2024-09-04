c1aa7db142f27823e8121cb926d3c04e711f44d6
BAGAS DOU ZUN <douzunbagas@gmail.com>
2024-09-04 12:56:10 +0700

    terupdate

--- notif_wa.php
+++ notif_wa.php
@@ -1,7 +1,7 @@
   <?php
-  function kirim_notifikasi_wa($telepon, $nama, $email)
+  function kirim_notifikasi_wa($telepon, $name, $email)
   {
-    $curl = curl_int();
+    $curl = curl_init();
     curl_setopt_array($curl, array(
         CURLOPT_URL => 'https://api.fonnte.com/send',
         CURLOPT_RETURNTRANSFER => true,
@@ -12,18 +12,19 @@
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'POST',
         CURLOPT_POSTFIELDS => array(
-            'target' => @telepon,
-            'message' => "Nama Anda : *$nama* dan Email : *$email*
-            sudah mengisi form.\n\n_send via Chatbot-Sterida_",
+            'target' => $telepon,
+            'message' => "Nama Anda : $name dan Email : $email",
             'countrycode' => '62',
         ),
         CURLOPT_HTTPHEADER => array(
-            'Authorization : a_x3btP8b3ASn3ohZ7oE'
+            'Authorization: a_x3btP8b3ASn3ohZ7oE'
         ),
     ));
     $response = curl_exec($curl);
     if ($response === false) {
         echo 'curl error : ' . curl_error ($curl);
+    } else {
+        echo 'response: ' . $response;
     }
     curl_close ($curl);
   }
@@ -32,16 +33,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $telepon = htmlspecialchars($_POST['telepon']);
-    $whatsapp = htmlspecialchars($_POST['whatsapp']);
+    //$whatsapp = htmlspecialchars($_POST['whatsapp']);
 
     echo "<h2>Data Pendaftaran</h2>";
-    echo "<p><strong>Nama :</strong></p>";
-    echo "<p><strong>Email :</strong></p>";
+    echo "<p><strong>Nama : </strong> $name</p>";
+    echo "<p><strong>Email :</strong> $email</p>";
     echo "<p><strong>Nomor Telepon :</strong> $telepon</p>";
 
     kirim_notifikasi_wa($telepon, $name, $email);
 } else {
-    header("location : register.html");
+    header("location: register.html");
     exit();
 }
 

7d7f7713700b27c360466264c5af1f9571b630dc
BAGAS DOU ZUN <douzunbagas@gmail.com>
2024-09-05 09:35:25 +0700

    terupdate

--- list_siswa.php
+++ list_siswa.php
@@ -2,9 +2,9 @@
 include 'koneksi.php';
 
 $sql = "SELECT id, nama, absen, kelas, email, telepon, alamat, tanggal_daftar FROM siswa_tabel";
-$result =m $conn->query($sql);
+$result = $conn->query($sql);
 
-if ($result->numb_rows > 0) {
+if ($result->num_rows > 0) {
   echo "<table border='1'>";
   echo "<tr>
   <th>ID</th>
@@ -14,6 +14,31 @@ if ($result->numb_rows > 0) {
   <th>Email</th>
   <th>Telepon</th>
   <th>Alamat</th>
-  <th>Tanggal Daftar</th>"
+  <th>Tanggal Daftar</th>";
+
+while($row = $result->fetch_assoc()) {
+  echo "<tr>";
+  echo "<td>" . $row["id"] . "</td>";
+  echo "<td>" . $row["nama"] . "</td>";
+  echo "<td>" . $row["absen"] . "</td>";
+  echo "<td>" . $row["kelas"] . "</td>";
+  echo "<td>" . $row["email"] . "</td>";
+  echo "<td>" . $row["telepon"] . "</td>";
+  echo "<td>" . $row["alamat"] . "</td>";
+  echo "<td>" . $row["tanggal_daftar"] . "</td>";
+
+  echo "<td><a href='edit.php?id=" . $row["id"] . "'Edit</a></td>";
+  echo "<td><a href='delete.php?id=" . $row["id"] . "'onclick='return confirm(\"Apakah Anda Yakin Ingin Menghapus Data Ini?\")'Delete</a></td>";
+
+  echo "</tr>";
 }
+
+echo "</table>";
+} else {
+  echo "Tidak Ada Data Ditemukan.";
+}
+
+echo "<td><a a href='register.html'>Tambah Data</a></td>";
+
+$conn->close();
 ?>

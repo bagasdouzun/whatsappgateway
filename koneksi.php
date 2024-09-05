c07ccf68864b9a1a23d1de9624348a245d65ce88
BAGAS DOU ZUN <douzunbagas@gmail.com>
2024-09-05 07:28:46 +0700

    terupdate

---
+++ koneksi.php
@@ -0,0 +1,79 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = htmlspecialchars($_POST['name']);
+  $absen = htmlspecialchars($_POST['absen']);
+  $kelas = htmlspecialchars($_POST['kelas']);
+  $email = htmlspecialchars($_POST['email']);
+  $telepon = htmlspecialchars($_POST['telepon']);
+  $alamat = htmlspecialchars($_POST['alamat']);
+
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "form_reg";
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  if ($conn->connect_error) {
+    die("Koneksi Gagal: " . $conn->connect_error);
+  }
+
+  $sql = "INSERT INTO siswa_tabel (nama, absen, kelas, email, telepon, alamat)
+  VALUES ('$name', '$absen', '$kelas', '$email', '$telepon', '$alamat')";
+
+  if ($conn->query($sql) === TRUE) {
+  echo "Data Berhasil Disimpan.";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+  $conn->close();
+  } else {
+  header("location : register.html");
+  exit();
+}
+
+include 'notif_wa.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proses Pendaftaran</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f4f4f4;
+        padding: 20px;
+      }
+      .container {
+        max-width: 600px;
+        margin: auto;
+        background: #fff;
+        padding-top: 30px;
+        padding-bottom: 40px;
+        padding-right: 50px;
+        padding-left: 30px;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      }
+      h1 {
+        text-align: center;
+        margin-bottom: 50px;
+      }
+    </style>
+</head>
+<body>
+    <div class="container">
+    <h1>Data Pendaftaran</h1>
+    <p><strong>Nama :</strong> <?php echo $name; ?></p>
+    <p><strong>Kelas :</strong> <?php echo $kelas; ?></p>
+    <p><strong>Absen :</strong> <?php echo $absen; ?></p>
+    <p><strong>Email :</strong> <?php echo $email; ?></p>
+    <p><strong>Nomor Telepon :</strong> <?php echo $telepon; ?></p>
+    <p><strong>Alamat :</strong> <?php echo $alamat; ?></p>
+    </div>
+</body>
+</html>
